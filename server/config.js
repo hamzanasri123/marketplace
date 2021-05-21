9ad462f98ca50e3e43b99ba2718659a9104d854d
hamza nasri <hamzanasri16@gmail.com>
2021-05-21 09:41:09 +0100

    marketplace

---
+++ server/config.js
@@ -0,0 +1,9 @@
+//All the configurations required like portNumber, Database Connection URl etc are stored to provide a low level encapsulation
+
+module.exports = {
+  database:
+    'mongodb://localhost:27017/ecommerce',
+  port: 3000,
+  secret: 'RamtejaRepaka2012312321'
+};
+
